0169ee0cc181430bb1d9a90e6c0a203ec6c095bd
Muhammad Zubair <zubair812@gmail.com>
2019-08-01 19:56:51 -0400

    Removed default files and migrations & added new migration

--- database/migrations/2019_07_31_011027_create_calculations.php
+++ database/migrations/2019_07_31_011027_create_calculations.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateCalculations extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('calculations', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->double('first_operand', 15, 8);
+            $table->double('second_operand', 15, 8);
+            $table->char('operator', 1);
+            $table->double('result', 15, 8);
             $table->timestamps();
         });
     }
@@ -31,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('calculations');
     }
 }
